ca39a366329d65e68f25ffe920b09149555ef43b
emquigs <emily.s.quigley@gmail.com>
2014-02-20 16:36:27 -0500

    Drew dog and birds

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -17,10 +17,20 @@ function draw() {
 		var img = new Image();
 		img.onload = function() {
 			// Draw tree
-			ctx.drawImage(img, 0, 270, 88, 125, 50, 70, 281, 400);
+			ctx.drawImage(img, 0, 270, 88, 125, 30, 70, 281, 400);
 			
 			// Draw grass/road/bushes
 			ctx.drawImage(img, 0, 720, 900, 180, 0, 400, 800, 100);
+
+			// Draw sniffing dog
+			ctx.drawImage(img, 0, 0, 60, 44, 170, 400, 120, 88);
+
+			// Draw 5 birds
+			ctx.drawImage(img, 0, 115, 40, 40, 500, 300, 80, 80);
+			ctx.drawImage(img, 40, 150, 40, 40, 400, 150, 80, 80);
+			ctx.drawImage(img, 130, 150, 40, 40, 300, 50, 80, 80);
+			ctx.drawImage(img, 206, 110, 40, 40, 600, 40, 80, 80);
+			ctx.drawImage(img, 295, 193, 40, 40, 600, 200, 80, 80);
 		};
 		img.src = 'assets/duckhunt.png';
 	}
