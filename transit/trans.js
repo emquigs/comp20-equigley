8ad2f01a0e41945893ff8bb05adb79a2669321fd
emquigs <emily.s.quigley@gmail.com>
2014-03-12 14:59:42 -0400

    Wrote init function

--- transit/trans.js
+++ transit/trans.js
@@ -2,6 +2,21 @@
 var lat = -99999;
 var lng = -99999;
 
+var me = new google.maps.LatLng(lat, lng);
+var myOptions = {
+                    zoom: 8,
+                    center: me,
+};
+
+var map;
+var marker;
+var infowindow = new google.maps.InfoWindow();
+
+function init() {
+    map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+    getMyLocation();
+    }
+
 function getMyLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
@@ -12,9 +27,23 @@ function getMyLocation() {
     else {
         alert("Sorry, geolocation is not supported by your web browser.  -MGMT");
     }
-    initMap();
+    renderMap();
 }
 
-function initMap() {
-    
+function renderMap() {
+    me = new google.maps.LatLng(lat, lng);
+
+    map.panTo(me);
+
+    marker = new google.maps.Marker({
+            position: me,
+            title: "Here I Am!"
+        });
+    marker.setMap(map);
+
+    google.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent(marker.title);
+            infowindow.open(map, marker);
+        });
+
 }
